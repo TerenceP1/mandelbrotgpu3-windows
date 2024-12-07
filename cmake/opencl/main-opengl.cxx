58936920f8dcd966551a3b479049c2f9f83ec8d1
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-12-07 18:16:31 -0500

    Update main-opengl.cxx

--- cmake/opencl/main-opengl.cxx
+++ cmake/opencl/main-opengl.cxx
@@ -220,7 +220,7 @@ int main()
         2,
         sizeof(cl_mem),
         &c);
-    size_t zzz = 10000000;
+    size_t zzz = 100000000;
     cout << "kernel!!!" << clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &zzz, NULL, NULL, NULL, NULL)
          << endl;
     // Sleep(15000);
